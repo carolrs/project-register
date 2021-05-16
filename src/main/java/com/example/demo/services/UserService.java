1959903f40d026d723d439a62e76a0e9d983b50e
Carolina R <anacarolinarodriguesoares@gmail.com>
2021-05-16 11:21:37 +0100

    Exception handling - delete

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -2,8 +2,11 @@ package com.example.demo.services;
 
 import com.example.demo.entities.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.services.services.exceptions.DatabaseException;
 import com.example.demo.services.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
         return repository.save(obj);
     }
     public void delete (Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
     public User update (Long id, User obj){
         User entity = repository.getOne(id); //instancia o usuario. Não vai ao banco de dados.
